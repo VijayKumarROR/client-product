a14286eeaa0a9021f780c474ec6415738f148f76
VijayKumarROR <vijivs055@gmail.com>
2017-11-12 20:18:23 +0530

    added-small-changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  #root 'categories#new'
+
+  resources :categories
   devise_for :users
+  devise_scope :user do
+    authenticated :user do
+    root :to => 'categories#new', as: :authenticated_root
+  end
+  unauthenticated :user do
+    root :to => 'devise/registrations#new', as: :unauthenticated_root
+  end
+end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171112132500) do
+ActiveRecord::Schema.define(version: 20171112133651) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.text     "about"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
